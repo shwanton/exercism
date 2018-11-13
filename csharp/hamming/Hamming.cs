2bc69be3a6d18fed5e82cc84cf408fee035bd758
Shawn Dempsey <shwanton@gmail.com>
2018-11-13 11:41:50 -0800

    Submit csharp Hamming v3
    Move length checking to Func

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -5,16 +5,11 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
-        if (firstStrand.Length != secondStrand.Length)
-        {
-            throw new ArgumentException();
-        }
-
         return firstStrand.Where(CharactersAtIndexAreDifferent(secondStrand)).Count();
     }
 
     private static Func<char, int, bool> CharactersAtIndexAreDifferent(string strand)
     {
-        return (char letter, int index) => letter != strand[index];
+        return (char letter, int index) => strand.Length > index && letter != strand[index];
     }
 }
