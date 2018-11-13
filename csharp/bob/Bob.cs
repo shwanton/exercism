2938b235e5c253ad3bd6b1c550d8b7c72bb96237
Shawn Dempsey <shwanton@gmail.com>
2018-11-13 14:21:13 -0800

    Submit csharp Bob v1
    Regex implementation

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,9 +1,22 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var says = statement.Trim();
+
+        bool silent = String.IsNullOrEmpty(says);
+        bool askingQuestion = Regex.IsMatch(says, @".*\?+$");
+        bool yelling = says == says.ToUpper() && Regex.IsMatch(says, @"[A-Z]");
+        bool yellingQuestion = yelling && askingQuestion;
+
+        if (silent) return "Fine. Be that way!";
+        if (yellingQuestion) return "Calm down, I know what I'm doing!";
+        if (yelling) return "Whoa, chill out!";
+        if (askingQuestion) return "Sure.";
+
+        return "Whatever.";
     }
 }
