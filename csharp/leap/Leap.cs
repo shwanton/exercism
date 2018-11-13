4b5aad1f24405e91db3b382a25735d3723a948fa
Shawn Dempsey <shwanton@gmail.com>
2018-11-13 10:42:37 -0800

    Submit csharp leap

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,6 +4,10 @@ public static class Leap
 {
     public static bool IsLeapYear(int year)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var every4Years = year % 4 == 0;
+        var every100Years = year % 100 == 0;
+        var every400Years = year % 400 == 0;
+
+        return every4Years && (every400Years || !every100Years);
     }
 }
