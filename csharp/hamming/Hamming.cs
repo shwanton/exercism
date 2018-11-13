72d60de3807dfb4366e30c4cbd79fe39e6b0b7da
Shawn Dempsey <shwanton@gmail.com>
2018-11-13 11:44:30 -0800

    Revert removing custom error assertion

--- csharp/hamming/Hamming.cs
+++ csharp/hamming/Hamming.cs
@@ -5,6 +5,11 @@ public static class Hamming
 {
     public static int Distance(string firstStrand, string secondStrand)
     {
+        if (firstStrand.Length != secondStrand.Length)
+        {
+            throw new ArgumentException();
+        }
+
         return firstStrand.Where(CharactersAtIndexAreDifferent(secondStrand)).Count();
     }
 
