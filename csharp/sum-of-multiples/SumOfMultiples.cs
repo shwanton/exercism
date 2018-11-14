5550fce8aca7d85a0a2823f116a5bfc6e176e106
Shawn Dempsey <shwanton@gmail.com>
2018-11-13 16:31:35 -0800

    Submit csharp sum-of-multiples v1
    For loops
    Aggregate results

--- csharp/sum-of-multiples/SumOfMultiples.cs
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using static System.Linq.Enumerable;
 
 public static class SumOfMultiples
 {
     public static int Sum(IEnumerable<int> multiples, int max)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        HashSet<int> results = new HashSet<int>();
+
+        foreach (var multiple in multiples.Where(n => n > 0))
+        {
+            foreach (var num in Range(1, max - 1))
+            {
+                if (num > 0 && num % multiple == 0)
+                {
+                    results.Add(num);
+                }
+            }
+        }
+
+        return results.Count == 0 ? 0 : results.Aggregate((sum, value) => sum + value);
     }
 }
+
