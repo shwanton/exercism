171202ae3613a402d4a79cef099dd401950117e1
Shawn Dempsey <shwanton@gmail.com>
2018-11-15 05:30:28 -0800

    Download csharp two-fer

---
+++ csharp/two-fer/TwoFer.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+  public static class TwoFer
+  {
+      public static string Name(string input = null)
+      {
+          throw new NotImplementedException("You need to implement this function.");
+      }
+  }
