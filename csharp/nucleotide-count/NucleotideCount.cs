6a6c5335b9d160ebaed43b670fd091d00ec0e85d
Shawn Dempsey <shwanton@gmail.com>
2018-11-16 06:36:49 -0800

    Submit csharp nucleotide-count v1

--- csharp/nucleotide-count/NucleotideCount.cs
+++ csharp/nucleotide-count/NucleotideCount.cs
@@ -1,10 +1,27 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class NucleotideCount
 {
+    private static char[] _strands = new char[] { 'A', 'C', 'G', 'T' };
+
     public static IDictionary<char, int> Count(string sequence)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        foreach (var s in sequence)
+        {
+            if (!_strands.Contains(s)) throw new ArgumentException();
+        }
+        return _strands.Aggregate(new Dictionary<char, int>(), FindStrands(sequence));
+    }
+
+    private static Func<Dictionary<char, int>, char, Dictionary<char, int>> FindStrands(string sequence)
+    {
+        return (acc, strand) =>
+        {
+            var result = sequence.Where(value => value == strand).Count();
+            acc[strand] = result > 0 ? result : 0;
+            return acc;
+        };
     }
 }
