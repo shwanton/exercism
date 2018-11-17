e4a3d0a1669636c14b9eaaf597585e7d01c28e35
Shawn Dempsey <shwanton@gmail.com>
2018-11-16 19:37:09 -0800

    Submit dotnet collatz-conjecture v1
    for each version

--- csharp/collatz-conjecture/CollatzConjecture.cs
+++ csharp/collatz-conjecture/CollatzConjecture.cs
@@ -4,6 +4,17 @@ public static class CollatzConjecture
 {
     public static int Steps(int number)
     {
-        throw new NotImplementedException("You need to implement this function");
+        if (number < 1) throw new ArgumentException();
+
+        var result = 0;
+
+        while (number > 1)
+        {
+            result++;
+
+            number = number % 2 == 0 ? number / 2 : number * 3 + 1;
+        }
+
+        return result;
     }
 }
