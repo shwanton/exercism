fdd261d25a3121c72fe0da0e5a0d4b81515985f7
Shawn Dempsey <shwanton@gmail.com>
2018-11-17 16:29:41 -0800

    Submit dotnet armstrong-numbers v1
    Incomplete

--- csharp/armstrong-numbers/ArmstrongNumbers.cs
+++ csharp/armstrong-numbers/ArmstrongNumbers.cs
@@ -1,9 +1,22 @@
 ﻿using System;
+using System.Linq;
 
 public static class ArmstrongNumbers
 {
     public static bool IsArmstrongNumber(int number)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var str = number.ToString();
+        var result = 0;
+
+        foreach (var num in str)
+        {
+            int test = Convert.ToInt16(num);
+            result = test * str.Length;
+            Console.WriteLine($"num: {test}, result: {str.Length}");
+        }
+
+        Console.WriteLine($"num: {number}, result: {result}");
+
+        return result == number;
     }
 }
