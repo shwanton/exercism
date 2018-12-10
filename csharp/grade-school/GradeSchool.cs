a7112e24d7f4b471794ecfe84fe08ca4de09afda
Shawn Dempsey <shwanton@gmail.com>
2018-12-10 09:34:23 -0800

    Submit csharp grade-school v1

--- csharp/grade-school/GradeSchool.cs
+++ csharp/grade-school/GradeSchool.cs
@@ -1,20 +1,42 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public class GradeSchool
 {
+    private readonly List<Student> _students = new List<Student>();
+
     public void Add(string student, int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        _students.Add(new Student(student, grade));
     }
 
     public IEnumerable<string> Roster()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return _students
+            .OrderBy(s => s.Grade)
+            .ThenBy(s => s.Name)
+            .Select(s => s.Name);
     }
 
     public IEnumerable<string> Grade(int grade)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return _students
+            .Where(s => s.Grade == grade)
+            .OrderBy(s => s.Grade)
+            .ThenBy(s => s.Name)
+            .Select(s => s.Name);
+    }
+}
+
+public class Student
+{
+    public String Name;
+    public int Grade;
+
+    public Student(string name, int grade)
+    {
+        Name = name;
+        Grade = grade;
     }
 }
